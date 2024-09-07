756cf54faa72a49c3db5c92089b55f17d8faaeda
Bikatr7 <Bikatr7@proton.me>
2024-09-07 14:18:29 -0600

    Added frontend base files

--- untitled-hotel-manager/backend/main.py
+++ untitled-hotel-manager/backend/main.py
@@ -1,3 +1,7 @@
+## Copyright [name placeholder] (https://github.com/Bikatr7/CS3300GroupProject) ([url placeholder])
+## Use of this source code is governed by an GNU Affero General Public License v3.0
+## license that can be found in the LICENSE file.
+
 ## built-in libraries
 from datetime import datetime, timedelta, timezone
 import os
---
+++ untitled-hotel-manager/frontend/vite.config.ts
@@ -0,0 +1,30 @@
+// Copyright [name placeholder] (https://github.com/Bikatr7/CS3300GroupProject) ([url placeholder])
+// Use of this source code is governed by an GNU Affero General Public License v3.0
+// license that can be found in the LICENSE file.
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => 
+{
+
+  if(mode === 'production') 
+  {
+    process.env.NODE_ENV = 'production';
+  } 
+  else 
+  {
+    process.env.NODE_ENV = 'development';
+  }
+
+  return {
+    plugins: [react()],
+    build: {
+      sourcemap: true,
+    },
+    server: {
+      port: 5173, 
+    },
+  }
+})
