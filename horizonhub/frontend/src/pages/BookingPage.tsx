e82e643f726a4c9bb6328bc85a6c5c12b7c2e2e8
Bikatr7 <Bikatr7@proton.me>
2024-11-01 20:44:38 -0600

    fixed dev issues

--- horizonhub/frontend/src/pages/BookingPage.tsx
+++ horizonhub/frontend/src/pages/BookingPage.tsx
@@ -6,63 +6,48 @@
 
 // chakra-ui
 import {
-    VStack,
-    HStack,
-    Heading,
-    Box,
-    Text,
     Button,
     Container,
-    Flex,
-    SimpleGrid,
-    GridItem,
-    FormControl,
-    FormLabel,
-    Input,
-    Select,
-    Checkbox
+    Flex
   } from "@chakra-ui/react";
-  import { DatePicker, Stack } from 'rsuite';
-  //import { ReactDOM } from "react";
-  import { useNavigate } from "react-router-dom";
-  //import * as React from 'react';
-  import { useState } from "react";
-  import { DateRangePicker } from "react-date-range";
-  import "react-date-range/dist/styles.css";
-  import "react-date-range/dist/theme/default.css"
 
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { DateRangePicker } from "@wojtekmaj/react-daterange-picker";
+import "@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css";
+import "react-calendar/dist/Calendar.css";
 
-  function BookingPage() {
-
+function BookingPage() 
+{
     let navigate = useNavigate();
-    const pageChange = () =>{
+    const pageChange = () =>
+    {
       let path = `checkout`;
       navigate(path);
     }
 
-    const [date, setDate] = useState(
-      {
-        startDate: new Date(),
-        endDate: new Date(),
-        key: 'selection'
-      }
-    );
+    const [dateRange, setDateRange] = useState([new Date(), new Date()]);
       
     return (
       <Container maxW="container.xl" p={0}>
-
-        <Flex h="60vh" py={20} display="flex">
-          <span className="calendar" color="#2e343c"></span>
-          <DateRangePicker className='picker' ranges={[date]} onChange={()=>{}} 
-          >
-
-          </DateRangePicker>
+        <Flex h="60vh" py={20} display="flex" justifyContent="center">
+          <DateRangePicker 
+            value={dateRange}
+            onChange={setDateRange}
+            format="y-MM-dd"
+          />
         </Flex>
-        <Button colorScheme="orange" size="lg" w="full" alignSelf="center" 
-            onClick={pageChange}>Book Room</Button>
-        
+        <Button 
+          colorScheme="orange" 
+          size="lg" 
+          w="full" 
+          alignSelf="center" 
+          onClick={pageChange}
+        >
+          Book Room
+        </Button>
       </Container>
     );
-  }
+}
   
-  export default BookingPage;
+export default BookingPage;
